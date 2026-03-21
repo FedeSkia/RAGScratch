c7577e34e6b71bae1780b7a4bbf40f092436ad89
Federico Conoci <federico.conoci@musement.com>
2026-03-21 20:33:36 +0100

    added documentation

--- src/rag_app/ingestion/ingestor.py
+++ src/rag_app/ingestion/ingestor.py
@@ -8,7 +8,10 @@ LOADER_REGISTRY: dict[str, DocumentLoader] = {
     ".md": MDIngestor(),
 }
 
+
 def ingest_directory(directory: str) -> list[Document]:
+    ''' ingest a folder of files. Currently supports only ".md" files.
+     The code allows to add more files implementing the logic in a class like md_ingestor.py '''
     documents = []
     for file_path in Path(directory).rglob("*"):
         loader = LOADER_REGISTRY.get(file_path.suffix)
